b2c0810fd388e2d4053511a9a9fa4b3d5d7b2950
DjordjevicN <48070119+DjordjevicN@users.noreply.github.com>
2021-08-22 01:29:32 +0200

    added sidebar (#4)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Login from './components/pages/loginPage/Login'
 import HomePage from './components/pages/HomePage'
+import UserPage from './components/pages/userPage/UserPage'
 import SideMenu from './components/organisms/sideMenu/SideMenu'
 
 function App() {
@@ -19,6 +20,10 @@ function App() {
           <Switch>
             <Route path='/' exact component={HomePage} />
             <Route path='/login' component={Login} />
+            <Route path='/userPage' component={UserPage} />
+            <Route path='/blogPage' component={UserPage} />
+            <Route path='/animalPage' component={UserPage} />
+            <Route path='/bannerPage' component={UserPage} />
           </Switch>
         </div>
       </div>
---
+++ src/components/pages/userPage/UserPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function UserPage() {
+    return (
+        <div>
+            user
+        </div>
+    );
+}
+
+export default UserPage;
